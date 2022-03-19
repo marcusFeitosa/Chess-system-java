e747fafa025d0f4757962559f32b10a7ecdd7beb
Marcus Feitosa <marcus.feitosa_al@hotmail.com>
2022-03-19 18:27:52 -0300

    BoardException and defense programming

--- src/boardGame/Board.java
+++ src/boardGame/Board.java
@@ -9,6 +9,8 @@ public class Board {
 	public Board() {
 	}
 	public Board(int rows, int columns) {
+		if (rows<1||columns<1)
+			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
 		this.rows = rows;
 		this.columns = columns;
 		pieces=new Piece[rows][columns];
@@ -18,26 +20,27 @@ public class Board {
 	public int getRows() {
 		return rows;
 	}
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
+	
 	public int getColumns() {
 		return columns;
 	}
-	public void setColumn(int columns) {
-		this.columns = columns;
-	}
-	
+		
 	//Methods (no implemetation)
 	public Piece piece(int row, int column) {
+		if(!positionExists(row, column))
+			throw new BoardException("position not on the board!");
 		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position) {
+		if(!positionExists(position))
+			throw new BoardException("position not on the board!");
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece(Piece piece, Position position) {
+		if(thereIsAPiece(position)) 
+			throw new BoardException("There is already a piece on position "+position);
 		pieces[position.getRow()][position.getColumn()]=piece;
 		piece.position=position;
 	}
@@ -46,12 +49,18 @@ public class Board {
 		return pieces[0][0];
 	}
 	
-	public Boolean ExistsPiece(Position position) {
-		return true;
+	private boolean positionExists(int row, int column) {
+		return row>=0 && row<rows && column>=0 && column>=0 && column<columns;
 	}
 	
-	public Boolean thereIsAPierce(Position position) {
-		return true;
+	public Boolean positionExists(Position position) {
+		return positionExists(position.getRow(),position.getColumn());
+	}
+	
+	public Boolean thereIsAPiece(Position position) {
+		if(!positionExists(position))
+			throw new BoardException("position not on the board!");
+		return piece(position)!=null;
 	}
 	
 	
