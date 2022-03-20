e003e64cfad6f25016104c4f1ba60203f7f0b0fd
Marcus Feitosa <marcus.feitosa_al@hotmail.com>
2022-03-20 11:03:52 -0300

    Moving Pieces

--- src/boardGame/Board.java
+++ src/boardGame/Board.java
@@ -46,7 +46,16 @@ public class Board {
 	}
 	
 	public Piece removePiece(Position position) {
-		return null;
+		if(!positionExists(position))
+			throw new BoardException("position not on the board!");
+		if(piece(position)==null) {
+			return null;
+			
+		}
+		Piece aux=piece(position);
+		aux.position=null;
+		pieces[position.getRow()][position.getColumn()]=null;
+		return aux;
 	}
 	
 	private boolean positionExists(int row, int column) {
