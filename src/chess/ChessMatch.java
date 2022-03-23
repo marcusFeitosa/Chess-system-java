1570178eb3b466def6ee980768e1c866367b4c75
Marcus Feitosa <marcus.feitosa_al@hotmail.com>
2022-03-22 22:38:14 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -40,6 +40,9 @@ public class ChessMatch {
 	private void validateSourcePosition(Position position) {
 		if(!board.thereIsAPiece(position))
 			throw new ChessException("There is no piece on source position");
+	
+		if(!board.piece(position).IsThereAnyPossibleMove())
+			throw new ChessException("There is no possible moves for the chosen pieces");
 	}
 	private void placeNewPiece(char column, int row, ChessPiece pice) {
 		board.placePiece(pice, new ChessPosition(column,row).toPosition());
--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -16,4 +16,11 @@ public class Rook extends ChessPiece{
 		return "R";
 	}
 	
+	
+	@Override // por padrão um novo boolean é instaciado com todos os valores falsos
+	public boolean[][] possibleMoves() {
+		boolean[][] mat= new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
+	
 }
